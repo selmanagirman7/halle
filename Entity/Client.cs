4c90aa62d221f2e79e65ed63d3e818962b20bd6a
selman <88801652+selmanagirman7@users.noreply.github.com>
2023-01-04 15:30:44 +0300

    Add files via upload

---
+++ Entity/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+    }
+}
